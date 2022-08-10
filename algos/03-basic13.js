df93da3a6b2cbaeb817c8e7627e447253928f22c
sdahal1 <raabdahl@gmail.com>
2022-08-10 10:20:13 -0700

    flex lecture complete

---
+++ algos/03-basic13.js
@@ -0,0 +1,68 @@
+//print all the integers from 1-200
+
+// for(let i = 1; i<=200; i++ ){
+//     console.log(i);
+// }
+
+
+//print all the even numbers from 1-100
+// for(let i = 2; i<=100; i=i+2 ){
+//     console.log(i);
+// }
+
+//print all the odd numbers from 1-30
+// for(let i = 1; i<=30; i++ ){
+//     if(i % 2 === 1){
+//         console.log(i);
+//     }
+// }
+
+
+//print all the even numbers from 1-30
+// for(let i = 1; i<=30; i++ ){
+//     if(i % 2 === 0){
+//         console.log(i);
+//     }
+// }
+
+
+//print all the number from 1-30 that are divisible by 3
+// for(let i = 1; i<=30; i++ ){
+//     if(i % 3 === 0){
+//         console.log(i);
+//     }
+// }
+
+
+
+
+//Print integers from 0 to 255, and with each integer print the sum so far.
+
+//create a variable to store the sum so far and set it to 0
+// let sumSoFar = 0
+
+// //loop from 0 to 255
+// for(let i = 0; i<=255; i++){
+//     //add i to the sumSoFar
+//     sumSoFar += i;
+
+//     //print i and sum so far
+//     console.log(i,sumSoFar);
+
+// }
+
+
+
+//Iterate through a given array, printing each value.
+let favMeals = ["Calamari", "Sushi", "Steak", "Fried Chicken", "Lasagna"]; //elements are (calamari, sushi, steak, etc;)
+//                  0          1        2         3                4        //the indexes are the positions of each element in the array
+    
+
+// for(let i = 0; i< favMeals.length; i++){
+//     console.log(favMeals[i])
+// }
+
+// console.log(favMeals[0])
+// console.log(favMeals[1])
+// console.log(favMeals[2])
+
