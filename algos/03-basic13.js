db0db2a8ab48ab777f2d16f08d98921f0470f6ee
sdahal1 <raabdahl@gmail.com>
2022-08-11 08:47:24 -0700

    algos thursday

--- algos/03-basic13.js
+++ algos/03-basic13.js
@@ -54,7 +54,7 @@
 
 
 //Iterate through a given array, printing each value.
-let favMeals = ["Calamari", "Sushi", "Steak", "Fried Chicken", "Lasagna"]; //elements are (calamari, sushi, steak, etc;)
+//let favMeals = ["Calamari", "Sushi", "Steak", "Fried Chicken", "Lasagna"]; //elements are (calamari, sushi, steak, etc;)
 //                  0          1        2         3                4        //the indexes are the positions of each element in the array
     
 
@@ -65,4 +65,55 @@ let favMeals = ["Calamari", "Sushi", "Steak", "Fried Chicken", "Lasagna"]; //ele
 // console.log(favMeals[0])
 // console.log(favMeals[1])
 // console.log(favMeals[2])
+
+
+
+//Given an array, find and print its largest element.
+
+function findMax(arr){
+    let max = arr[0];
+    for(let i = 0; i< arr.length; i++){
+        //if the current value (arr[i]) is greater than our max, then update max to be the current value
+        if(arr[i] > max){
+            max = arr[i];
+        }
+    }
+    console.log(max);
+}
+
+
+// findMax([3,16,7,2])
+// findMax([1,2,3,4,20])
+
+
+//write a function that accepts an array and finds the sum of all the values in the given array
+
+function sumOfAll(arr){
+    let totalSum = 0;
+    //go through each and every element in the array and add that element to the total sum
+    for(let i = 0; i<arr.length; i++){
+        totalSum+= arr[i];
+    }
+
+    console.log('total sum from sum function', totalSum)
+    return totalSum
+}
+
+
+// sumOfAll([2,4,10,100]) //116;
+//        0 1 2  3
+
+
+function findAverage(arr){
+    //find the sum of all values in the array
+    let totalSum = sumOfAll(arr);
+    console.log('total sum in find average function', totalSum)
+    //divide the sum by the number of values in the array
+    let average = totalSum/arr.length;
+    console.log(average)
+}
+
+
+findAverage([10,20,30,40,50])
+
 
